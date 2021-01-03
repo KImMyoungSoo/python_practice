ceebe09fc27077c2a05db8de734ec9c6e03a5abf
KimMyoungSoo <5qndjddl6@naver.com>
2021-01-04 02:13:04 +0900

    study str

---
+++ section04-2.py
@@ -0,0 +1,17 @@
+# string
+
+str1 = "i am student"
+st2 = 'Niceman'
+
+print(len(str1), len(st2))
+
+a = 'Niceman'
+b = 'orange'
+
+print(a.islower())
+print(b.endswith('e'))
+print(a.replace('Nice','good'))
+
+print(a[0:2])
+print(b[0:4:2])
+print(b[::-1])
