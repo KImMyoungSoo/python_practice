54a914852bca4518d93379e254ab97b4fee11231
KimMyoungSoo <5qndjddl6@naver.com>
2021-01-03 18:23:40 +0900

    first commit

---
+++ section02.py
@@ -0,0 +1,23 @@
+# Section 02 -1
+
+# 기본출력
+print('Hello python !')
+
+# separator
+
+print('T','E','S','T', sep='')
+print('2020','02','02',sep='-')
+
+# end option
+
+print('Welcom to', end=' ')
+print('the blakc paradise', end=' ')
+print('piano noest')
+
+#format use
+
+print('{} and {}'.format('You','Me'))
+print('{0} and {1} and {0}'.format('You','Me'))
+print('{a} and {b}'.format(a='You',b='Me'))
+
+print("%s's favorite number is %d" % ('Ms',7))
